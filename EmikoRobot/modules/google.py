73a2865b7b991bcab3f974566bbbff9fdf325125
Si Arab - <98678420+fadhilabdat04@users.noreply.github.com>
2023-08-10 21:59:48 +0700

    Update google.py

--- EmikoRobot/modules/google.py
+++ EmikoRobot/modules/google.py
@@ -279,15 +279,15 @@ async def apk(e):
 __mod_name__ = "Search"
 
 __help__ = """
-❂ /google <query>*:* Perform a google search
-❂ /image <query>*:* Search Google for images and returns them\nFor greater no. of results specify lim, For eg: `/img hello lim=10`
-❂ /app <appname>*:* Searches for an app in Play Store and returns its details.
-❂ /reverse: Does a reverse image search of the media which it was replied to.
-❂ /gps <location>*:* Get gps location.
-❂ /github <username>*:* Get information about a GitHub user.
-❂ /country <country name>*:* Gathering info about given country
-❂ /imdb <Movie name>*:* Get full info about a movie with imdb.com
-❂ Emiko <query>*:* Emiko answers the query
+ᱵ /google <query>*:* Perform a google search
+ᱵ /image <query>*:* Search Google for images and returns them\nFor greater no. of results specify lim, For eg: `/img hello lim=10`
+ᱵ /app <appname>*:* Searches for an app in Play Store and returns its details.
+ᱵ /reverse: Does a reverse image search of the media which it was replied to.
+ᱵ /gps <location>*:* Get gps location.
+ᱵ /github <username>*:* Get information about a GitHub user.
+ᱵ /country <country name>*:* Gathering info about given country
+ᱵ /imdb <Movie name>*:* Get full info about a movie with imdb.com
+ᱵ Emiko <query>*:* Emiko answers the query
 
   💡Ex: `Emiko where is Japan?`
 """
