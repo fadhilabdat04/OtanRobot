6250cb2df26cac877b1fae3c1a1f03aed4980f7a
Si Arab - <98678420+fadhilabdat04@users.noreply.github.com>
2023-08-10 19:45:20 +0700

    Update admin.py

--- EmikoRobot/modules/admin.py
+++ EmikoRobot/modules/admin.py
@@ -968,30 +968,30 @@ def bug_reporting(update: Update, _: CallbackContext):
 
 __help__ = """
 *User Commands*:
-❂ /admins*:* list of admins in the chat
-❂ /pinned*:* to get the current pinned message.
+ᱵ /admins*:* list of admins in the chat
+ᱵ /pinned*:* to get the current pinned message.
 
 *The Following Commands are Admins only:* 
-❂ /pin*:* silently pins the message replied to - add `'loud'` or `'notify'` to give notifs to users
-❂ /unpin*:* unpins the currently pinned message
-❂ /invitelink*:* gets invitelink
-❂ /promote*:* promotes the user replied to
-❂ /fullpromote*:* promotes the user replied to with full rights
-❂ /demote*:* demotes the user replied to
-❂ /title <title here>*:* sets a custom title for an admin that the bot promoted
-❂ /admincache*:* force refresh the admins list
-❂ /del*:* deletes the message you replied to
-❂ /purge*:* deletes all messages between this and the replied to message.
-❂ /purge <integer X>*:* deletes the replied message, and X messages following it if replied to a message.
-❂ /setgtitle <text>*:* set group title
-❂ /setgpic*:* reply to an image to set as group photo
-❂ /setdesc*:* Set group description
-❂ /setsticker*:* Set group sticker
+ᱵ /pin*:* silently pins the message replied to - add `'loud'` or `'notify'` to give notifs to users
+ᱵ /unpin*:* unpins the currently pinned message
+ᱵ /invitelink*:* gets invitelink
+ᱵ /promote*:* promotes the user replied to
+ᱵ /fullpromote*:* promotes the user replied to with full rights
+ᱵ /demote*:* demotes the user replied to
+ᱵ /title <title here>*:* sets a custom title for an admin that the bot promoted
+ᱵ /admincache*:* force refresh the admins list
+ᱵ /del*:* deletes the message you replied to
+ᱵ /purge*:* deletes all messages between this and the replied to message.
+ᱵ /purge <integer X>*:* deletes the replied message, and X messages following it if replied to a message.
+ᱵ /setgtitle <text>*:* set group title
+ᱵ /setgpic*:* reply to an image to set as group photo
+ᱵ /setdesc*:* Set group description
+ᱵ /setsticker*:* Set group sticker
 
 *Rules*:
-❂ /rules*:* get the rules for this chat.
-❂ /setrules <your rules here>*:* set the rules for this chat.
-❂ /clearrules*:* clear the rules for this chat.
+ᱵ /rules*:* get the rules for this chat.
+ᱵ /setrules <your rules here>*:* set the rules for this chat.
+ᱵ /clearrules*:* clear the rules for this chat.
 """
 
 SET_DESC_HANDLER = CommandHandler("setdesc", set_desc, filters=Filters.chat_type.groups, run_async=True)
