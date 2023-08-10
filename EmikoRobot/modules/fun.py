e929511fdd86ea7dca227608724ed9cf162fa814
Si Arab - <98678420+fadhilabdat04@users.noreply.github.com>
2023-08-10 20:24:13 +0700

    Update fun.py

--- EmikoRobot/modules/fun.py
+++ EmikoRobot/modules/fun.py
@@ -313,39 +313,39 @@ def weebify(update: Update, context: CallbackContext):
 
 
 __help__ = """
-❂ /runs*:* reply a random string from an array of replies
-❂ /slap*:* slap a user, or get slapped if not a reply
-❂ /shrug*:* get shrug XD
-❂ /table*:* get flip/unflip :v
-❂ /decide*:* Randomly answers yes/no/maybe
-❂ /toss*:* Tosses A coin
-❂ /bluetext*:* check urself :V
-❂ /roll*:* Roll a dice
-❂ /rlg*:* Join ears,nose,mouth and create an emo ;-;
-❂ /shout <keyword>*:* write anything you want to give loud shout
-❂ /weebify <text>*:* returns a weebified text
-❂ /sanitize*:* always use this before /pat or any contact
-❂ /pat*:* pats a user, or get patted
-❂ /8ball*:* predicts using 8ball method
+ᱵ /runs*:* reply a random string from an array of replies
+ᱵ /slap*:* slap a user, or get slapped if not a reply
+ᱵ /shrug*:* get shrug XD
+ᱵ /table*:* get flip/unflip :v
+ᱵ /decide*:* Randomly answers yes/no/maybe
+ᱵ /toss*:* Tosses A coin
+ᱵ /bluetext*:* check urself :V
+ᱵ /roll*:* Roll a dice
+ᱵ /rlg*:* Join ears,nose,mouth and create an emo ;-;
+ᱵ /shout <keyword>*:* write anything you want to give loud shout
+ᱵ /weebify <text>*:* returns a weebified text
+ᱵ /sanitize*:* always use this before /pat or any contact
+ᱵ /pat*:* pats a user, or get patted
+ᱵ /8ball*:* predicts using 8ball method
 
 - Animation
-❂ /love 
-❂ /hack 
-❂ /bombs 
+ᱵ /love 
+ᱵ /hack 
+ᱵ /bombs 
 
 - Shippering
-❂ /couples - get couples of today
+ᱵ /couples - get couples of today
 
 - Here is the help for the Styletext module:
 
-❂ /weebify <text>: weebify your text!
-❂ /bubble <text>: bubble your text!
-❂ /fbubble <text>: bubble-filled your text!
-❂ /square <text>: square your text!
-❂ /fsquare <text>: square-filled your text!
-❂ /blue <text>: bluify your text!
-❂ /latin <text>: latinify your text!
-❂ /lined <text>: lined your text!
+ᱵ /weebify <text>: weebify your text!
+ᱵ /bubble <text>: bubble your text!
+ᱵ /fbubble <text>: bubble-filled your text!
+ᱵ /square <text>: square your text!
+ᱵ /fsquare <text>: square-filled your text!
+ᱵ /blue <text>: bluify your text!
+ᱵ /latin <text>: latinify your text!
+ᱵ /lined <text>: lined your text!
 """
 
 SANITIZE_HANDLER = DisableAbleCommandHandler("sanitize", sanitize, run_async=True)
